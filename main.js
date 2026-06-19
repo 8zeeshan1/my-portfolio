a09bf5e4a66d4def25ec3ed45ef2d28827cad966
MOHD Zeeshan Quraishi <zq8281@gmail.com>
2026-06-19 05:33:13 +0530

    added functionality

---
+++ main.js
@@ -0,0 +1,82 @@
+const navLinks = document.getElementById("nav-links");
+const menuBtn = document.getElementById("menu-btn");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__content .header__btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// about container
+ScrollReveal().reveal(".about__content .section__header", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".about__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__content .about__btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// education container
+ScrollReveal().reveal(".education__visual", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".education__item", {
+  ...scrollRevealOption,
+  interval: 250,
+});
+
+// skills container
+ScrollReveal().reveal(".skills__card", {
+  ...scrollRevealOption,
+  interval: 150,
+});
+
+// experience container
+ScrollReveal().reveal(".experience__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// portfolio container
+ScrollReveal().reveal(".portfolio__card", {
+  duration: 1000,
+  interval: 500,
+});
